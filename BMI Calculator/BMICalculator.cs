751428f3bff7b186095d23dbe4a37be88a1c4f73
Hüseyin Akturan <huseyiakturan@hotmail.com>
2016-08-18 11:08:10 +0200

    Initial commit

---
+++ BMI Calculator/BMICalculator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMI_Calculator
+{
+    public class BMICalculator
+    {
+        public double Height { get; set; }
+        public double Weight { get; set; }
+
+        private double _result { get; set; }
+
+        public double Calculate()
+        {
+            if (Height <= 0 || Weight <= 0)
+                throw new Exception("Please check Height and Weight params");
+
+            Height = Height / 100;
+
+            var _height = Height * Height;
+            var result = Math.Round(Weight / _height, 2);
+
+            _result = result;
+
+            return result;
+        }
+
+        public string Categories()
+        {
+            string categories = null;
+
+            if(_result > 0)
+            {
+                if (_result < 18.5)
+                    categories = "Underweight";
+                else if (_result > 18.4 && _result < 25)
+                    categories = "Normal weight";
+                else if (_result > 24.9 && _result < 30)
+                    categories = "Overweight";
+                else
+                    categories = "Obesity";
+            }
+
+            return categories;
+        }
+    }
+}
---
+++ BMITest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BMI_Calculator;
+
+namespace BMITest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BMICalculator calculator = new BMICalculator();
+            calculator.Height = 168; // Centimeter
+            calculator.Weight = 76; // Kilogram
+            var result = calculator.Calculate();
+
+            var _categories = calculator.Categories();
+
+            Console.WriteLine(result + " : " + _categories);
+            Console.ReadLine();
+        }
+    }
+}
